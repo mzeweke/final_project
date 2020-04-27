a551494efc25df2ebe21a97b28312b9d4ae4c65a
benjaminmillunchick <52924142+benjaminmillunchick@users.noreply.github.com>
2020-04-27 15:39:37 -0400

    Updated to include NBA

--- Attendace_Plots.py
+++ Attendace_Plots.py
@@ -4,7 +4,12 @@ import requests
 import os
 import pprint
 
-def reconds_and_attendace_nfl(league, cur, conn):
+def reconds_and_attendace(league, cur, conn):
+    ''' 
+    Joins the data from the two tables to prepare to be plotted. 
+    Takes a league as an imput, and outputs a list of JOINed data
+    '''
+
     path = os.path.dirname(os.path.abspath(__file__))
     db_conn = sqlite3.connect(path+'/'+"sports_records.db")
     db_cur = db_conn.cursor()
@@ -12,27 +17,42 @@ def reconds_and_attendace_nfl(league, cur, conn):
     db_cur.execute("SELECT * FROM sports_winloss")
     for row in db_cur:
         category_id_dict[row[1]] = row[0]
-
-    db_cur.execute('''SELECT DISTINCT sports_attend.name, sports_winloss.mascot, 
+    if league == 'NFL' or league == 'MLB' or league == 'NHL':
+        db_cur.execute('''SELECT DISTINCT sports_attend.name, sports_winloss.mascot, 
+                                        sports_attend.year, sports_attend.average, sports_attend.league,
+                                        sports_winloss.home_wins, sports_winloss.away_wins, 
+                                        sports_winloss.home_losses, sports_winloss.away_losses 
+                        FROM sports_attend 
+                        JOIN sports_winloss 
+                        ON sports_attend.name = sports_winloss.city  
+                        AND sports_attend.year = sports_winloss.year 
+                        WHERE sports_winloss.league= ?
+                        AND sports_attend.league = ?''', (league, league, ))
+    else:
+        db_cur.execute('''SELECT DISTINCT sports_attend.name, sports_winloss.mascot, 
                                       sports_attend.year, sports_attend.average, sports_attend.league,
                                       sports_winloss.home_wins, sports_winloss.away_wins, 
                                       sports_winloss.home_losses, sports_winloss.away_losses 
                       FROM sports_attend 
                       JOIN sports_winloss 
-                      ON sports_attend.name = sports_winloss.city  
+                      ON sports_attend.name = sports_winloss.mascot  
                       AND sports_attend.year = sports_winloss.year 
-                      WHERE sports_winloss.league= ?
-                      AND sports_attend.league = ?''', (league, league, ))
+                      WHERE sports_winloss.league= "NBA"
+                      AND sports_attend.league = "NBA"''')
     r_lsit = db_cur.fetchall()
     db_cur.close()
     return r_lsit
+   
 
 db_conn = sqlite3.connect("sports_records.db")
 db_cur = db_conn.cursor()
 
-nfl = reconds_and_attendace_nfl("NFL", db_cur, db_conn)
-
 def get_what_we_want(data):
+    '''
+    Formats the data collected from reconds_and_attendace_nfl() so that we can later plot. 
+    Input is the data. Outputs a dictionary of data
+    '''
+
     dic = {}
     for i in data:
         if i[0] == "New York" or i[0] == 'Los Angeles':
@@ -42,11 +62,12 @@ def get_what_we_want(data):
         dic[i[1]].append((i[3], i[5]+i[6]))
     return dic
 
-def plotting_nfl(league):
-    if league == 'NFL' or league == 'MLB' or league == 'NHL':
-        dic = get_what_we_want(reconds_and_attendace_nfl(league, db_cur, db_conn))
-    else:
-        return print("Can't do that quite yet")
+def plotting(league):
+    '''
+    Takes in a league that you wish to see data plotted, and calls other functions to create the plot.
+    '''
+
+    dic = get_what_we_want(reconds_and_attendace(league, db_cur, db_conn))
     for team in dic.keys():
         x = []
         y = []
@@ -63,4 +84,4 @@ def plotting_nfl(league):
     plt.show()
 
 ### Uncomment bellow and input a league
-#plotting_nfl("NHL")
+#plotting("NBA")
